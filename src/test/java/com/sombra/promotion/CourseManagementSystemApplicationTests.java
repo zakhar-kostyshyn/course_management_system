9386d60d12a5ffbc10d8c4098651360bb4d06c7f
zakhar-kostyshyn <zakharkostyshyn@gmail.com>
2022-02-07 23:48:36 +0200

    init

---
+++ src/test/java/com/sombra/promotion/CourseManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sombra.promotion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
