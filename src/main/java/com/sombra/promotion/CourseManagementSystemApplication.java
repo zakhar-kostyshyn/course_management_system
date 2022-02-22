e8543f07dc7f3063125716bcb287bf3a8e23763c
zakhar-kostyshyn <zakharkostyshyn@gmail.com>
2022-02-22 23:14:01 +0200

    add .env file, remove currently no need dependencies, add simple controller for testing purpose

--- src/main/java/com/sombra/promotion/CourseManagementSystemApplication.java
+++ src/main/java/com/sombra/promotion/CourseManagementSystemApplication.java
@@ -2,12 +2,27 @@ package com.sombra.promotion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class CourseManagementSystemApplication {
 
     public static void main(String[] args) {
+        System.out.println("Started");
         SpringApplication.run(CourseManagementSystemApplication.class, args);
+    }
+
+}
+
+
+@RestController
+class TestController {
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("It is working");
     }
 
 }
