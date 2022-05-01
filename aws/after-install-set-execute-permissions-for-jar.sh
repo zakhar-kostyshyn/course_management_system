ca5c86c28a5efc8efd8a8924a2db8ad76b4cda16
sombra-zahar-kostyshyn <zakhar.kostyshyn@sombrainc.com>
2022-05-01 23:16:56 +0300

    fix bugs in sh scripts

--- aws/after-install-set-execute-permissions-for-jar.sh
+++ aws/after-install-set-execute-permissions-for-jar.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-chmod +x ../build/libs/promotion-0.0.1-SNAPSHOT-plain.jar
+chmod +x /build/libs/promotion-0.0.1-SNAPSHOT-plain.jar
--- aws/application-start.sh
+++ aws/application-start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd .. && ./gradlew bootRun
+./gradlew bootRun
