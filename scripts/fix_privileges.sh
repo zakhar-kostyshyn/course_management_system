1fc095592c02992025568778dd7ad02ea36180d2
sombra-zahar-kostyshyn <zakhar.kostyshyn@sombrainc.com>
2022-05-04 21:47:25 +0300

    add buildspec.yml and changed logic for appspec.yml

---
+++ scripts/fix_privileges.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+chmod +x /app/course_management_system/* jar
+chmod +x /app/course_management_system/server_start.sh
+chmod +x /app/course_management_system/server_stop.sh
---
+++ scripts/server_clear.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+rm -rf /app/course_management_system
