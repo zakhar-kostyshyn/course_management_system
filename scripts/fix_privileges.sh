70ff7187c2c4b13de27b4534f9dfdc1aec2e3d13
sombra-zahar-kostyshyn <zakhar.kostyshyn@sombrainc.com>
2022-05-05 00:01:22 +0300

    add execute privileges to new files

--- scripts/fix_privileges.sh
+++ scripts/fix_privileges.sh
@@ -2,3 +2,5 @@
 chmod +x /app/course_management_system/* jar
 chmod +x /app/course_management_system/server_start.sh
 chmod +x /app/course_management_system/server_stop.sh
+chmod +x /app/course_management_system/server_pull_posgres_image.sh
+chmod +x /app/course_management_system/server_start_db.sh
