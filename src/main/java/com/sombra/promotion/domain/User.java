f05fcde75725bac96b38eb95fd5b5b113b630113
sombra-zahar-kostyshyn <zakhar.kostyshyn@sombrainc.com>
2022-05-07 14:28:30 +0300

    create start entities for domain

---
+++ src/main/java/com/sombra/promotion/domain/User.java
@@ -0,0 +1,17 @@
+package com.sombra.promotion.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    int id;
+
+}
