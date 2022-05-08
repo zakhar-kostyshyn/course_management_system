abfe2986ff38ed5181c910fd72316eb2d4811ded
sombra-zahar-kostyshyn <zakhar.kostyshyn@sombrainc.com>
2022-05-08 18:27:38 +0300

    replace springfox-boot-starter to springdoc-openapi-ui

--- src/main/java/com/sombra/promotion/configuration/SwaggerConfig.java
+++ src/main/java/com/sombra/promotion/configuration/SwaggerConfig.java
@@ -1,22 +1,24 @@
 package com.sombra.promotion.configuration;
 
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Course Management System API")
+                        .description("Help your learning business growth")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0"))
+                );
     }
 
 }
