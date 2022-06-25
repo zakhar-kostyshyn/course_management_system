a2437fab63c5e5f932ec12dbff3d294ea0dfd250
sombra-zahar-kostyshyn <zakhar.kostyshyn@sombrainc.com>
2022-06-26 00:50:13 +0300

    move env variables to jar call

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 cd /app/course_management_system || exit
-sudo java -jar promotion-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+sudo java -jar \
+ -DDATABASE_DB="$(aws ssm get-parameters --output text --region eu-central-1 --names DATABASE_DB --query Parameters[0].Value)" \
+ -DDATABASE_HOST="$(aws ssm get-parameters --output text --region eu-central-1 --names DATABASE_HOST --query Parameters[0].Value)" \
+ -DDATABASE_PASSWORD="$(aws ssm get-parameters --output text --region eu-central-1 --names DATABASE_PASSWORD --query Parameters[0].Value)" \
+ -DDATABASE_PORT="$(aws ssm get-parameters --output text --region eu-central-1 --names DATABASE_PORT --query Parameters[0].Value)" \
+ -DDATABASE_USERNAME="$(aws ssm get-parameters --output text --region eu-central-1 --names DATABASE_USERNAME --query Parameters[0].Value)" \
+ -DSPRING_SERVER_PORT="$(aws ssm get-parameters --output text --region eu-central-1 --names SPRING_SERVER_PORT --query Parameters[0].Value)" \
+ -DSWAGGER_URL="$(aws ssm get-parameters --output text --region eu-central-1 --names SWAGGER_URL --query Parameters[0].Value)" \
+ promotion-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
