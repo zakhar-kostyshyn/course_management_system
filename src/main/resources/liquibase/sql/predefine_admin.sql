dd11d20c382a70df1eccf28500b05b9f67eb8d59
zakhar-kostyshyn <zakharkostyshyn@gmail.com>
2022-07-22 23:59:42 +0300

    add ofFail for liquibase changeSet and replace predefine admin password from base64 to bcrypt

--- src/main/resources/liquibase/sql/predefine_admin.sql
+++ src/main/resources/liquibase/sql/predefine_admin.sql
@@ -3,6 +3,6 @@ do $$
         created_user_id uuid := uuid_generate_v4();
         admin_id uuid := (select id from role where name = 'admin');
     begin
-        insert into "user"(id, username, password) values (created_user_id, 'admin001', 'admin001');
+        insert into "user"(id, username, password) values (created_user_id, 'admin001', '$2a$12$RMF1ryShMhe2ABNwkobZF.ZIGniLf9EIG6bQMiBSEjGCtzKDV5eLO');
         insert into user_role(role_id, user_id, predefined) values (admin_id, created_user_id, true);
     end $$;
