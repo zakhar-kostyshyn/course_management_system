39e861c9ad5c2a0e9592c2b851eb7de33151e37c
zakhar-kostyshyn <zakharkostyshyn@gmail.com>
2022-09-07 12:51:04 +0300

    Use dateUtil in JwtTokenService

--- src/main/java/com/sombra/promotion/security/service/JwtTokenService.java
+++ src/main/java/com/sombra/promotion/security/service/JwtTokenService.java
@@ -3,6 +3,7 @@ package com.sombra.promotion.security.service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.sombra.promotion.service.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
@@ -15,9 +16,10 @@ import java.time.temporal.ChronoUnit;
 public class JwtTokenService {
 
     private final Algorithm algorithm;
+    private final DateUtil dateUtil;
 
     public String generateToken(String username) {
-        Instant issuedAt = Instant.now();
+        Instant issuedAt = dateUtil.nowInstant();
         return JWT.create()
                 .withIssuer("auth0")
                 .withSubject(username)
