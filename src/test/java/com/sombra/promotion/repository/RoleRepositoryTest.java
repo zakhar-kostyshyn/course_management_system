42f07b6c6682324ac534fbfee8921b1535d8a611
zakhar-kostyshyn <zakharkostyshyn@gmail.com>
2022-09-11 02:57:58 +0300

    integration test for RoleRepository

---
+++ src/test/java/com/sombra/promotion/repository/RoleRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.sombra.promotion.repository;
+
+import com.sombra.promotion.repository.abstraction.DatabaseIntegrationTest;
+import com.sombra.promotion.tables.pojos.Role;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static com.sombra.promotion.enums.RoleEnum.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+class RoleRepositoryTest extends DatabaseIntegrationTest {
+
+    @Autowired private RoleRepository roleRepository;
+
+    @Test
+    void must_required_role_by_role_enum() {
+
+        // act
+        Role studentRole = roleRepository.requiredByRoleEnum(student);
+        Role instructorRole = roleRepository.requiredByRoleEnum(instructor);
+        Role adminRole = roleRepository.requiredByRoleEnum(admin);
+
+        // verify
+        assertThat(studentRole.getName(), is(student));
+        assertThat(instructorRole.getName(), is(instructor));
+        assertThat(adminRole.getName(), is(admin));
+
+    }
+
+}
