6410284a9f20600507006004c9f62a30a87bfbdf
zakhar-kostyshyn <zakharkostyshyn@gmail.com>
2022-09-17 01:29:02 +0300

    improve messaging for healthcheck script

--- scripts/health_check.sh
+++ scripts/health_check.sh
@@ -3,9 +3,10 @@ while true
 do
     if [ "$(curl -s http://localhost:8088/healthcheck)" = 'Course Management System Works' ]
     then
+        echo "Healthcheck passed"
         exit 0
     else
-        echo "check server is running?"
-        sleep 3s
+        echo "Server is not running..."
+        sleep 10s
     fi
 done
